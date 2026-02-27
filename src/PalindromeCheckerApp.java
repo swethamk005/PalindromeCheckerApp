54539aeabe03de4d67fdc242fa268ee88c656baa
swethamk005 <swethamk005@gmail.com>
2026-02-27 14:55:00 +0530

    US5: Stack based palindrome checker using LIFO principle

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,38 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a string to check palindrome:");
         String input = scanner.nextLine();
-        char[] characters = input.toCharArray();
-        int start = 0;
-        int end = characters.length - 1;
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
         boolean isPalindrome = true;
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
+
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome");
         } else {
             System.out.println(input + " is NOT a Palindrome");
         }
+
         scanner.close();
     }
 }
