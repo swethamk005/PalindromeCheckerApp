d015fa4ef13c7c8e5231c044561f3cdba93f910c
swethamk005 <swethamk005@gmail.com>
2026-03-09 15:06:00 +0530

    UC10: Case-insensitive and space ignored Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,30 +1,30 @@
 public class PalindromeCheckerApp {
 
-    // Recursive method to check palindrome
-    public static boolean isPalindrome(String str, int start, int end) {
+    public static boolean isPalindrome(String input) {
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
-        // Base condition
-        if (start >= end) {
-            return true;
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
-        // If characters don't match
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
     }
 
     public static void main(String[] args) {
 
-        String input = "racecar";
+        String input = "Never Odd Or Even";
 
-        if (isPalindrome(input, 0, input.length() - 1)) {
-            System.out.println(input + " is a Palindrome");
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a Palindrome");
         } else {
-            System.out.println(input + " is NOT a Palindrome");
+            System.out.println("\"" + input + "\" is NOT a Palindrome");
         }
     }
 }
