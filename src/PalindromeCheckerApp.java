4a75d44b4a3f81b479f70847f54e7a0ddccff59e
swethamk005 <swethamk005@gmail.com>
2026-03-09 14:54:42 +0530

    UC8: Palindrome Checker App using Linked List

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,78 @@
-import java.util.Scanner;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
+    public static Node createList(String str) {
+        Node head = null;
+        Node tail = null;
+
+        for (char c : str.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String input = "madam";
 
-        System.out.println("Enter a string to check palindrome:");
-        String input = scanner.nextLine();
+        Node head = createList(input);
 
-        String reversed = "";
-
-        // Reverse string using for loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        // Compare original and reversed
-        if (input.equals(reversed)) {
+        if (isPalindrome(head))
             System.out.println(input + " is a Palindrome");
-        } else {
+        else
             System.out.println(input + " is NOT a Palindrome");
-        }
-
-        scanner.close();
     }
 }
